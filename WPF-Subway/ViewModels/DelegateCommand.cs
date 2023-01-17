6bb11b6502eab08d26b746db9d388f4d1a5f53d8
JooJh1225 <56584939+JooJh1225@users.noreply.github.com>
2023-01-17 17:17:09 +0900

    Feat: Initial commit

---
+++ WPF-Subway/ViewModels/DelegateCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPF_Subway.ViewModels
+{
+    internal class DelegateCommand : ICommand
+    {
+        //커맨드 바인딩을 위한 클래스
+
+        private readonly Func<bool> canExecute;
+        private readonly Action execute;
+
+        public DelegateCommand(Action execute) : this(execute, null)
+        {
+
+        }
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object o)
+        {
+            if(this.canExecute == null)
+            {
+                return true;
+            }
+            return this.canExecute();
+        }
+
+        public void Execute(object o)
+        {
+            this.execute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            if(this.CanExecuteChanged != null)
+            {
+                this.CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ WPF-Subway/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Subway.ViewModels
+{
+    class MainWindowViewModel : Notifier
+    {
+        public MainWindowViewModel()
+        {
+        }
+        private string _bindintTest;
+        public string bindingTest { get => _bindintTest; set { _bindintTest = value; OnPropertyChanged(nameof(bindingTest)); }}
+                            
+    }
+}
